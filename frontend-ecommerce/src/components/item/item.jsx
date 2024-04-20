888ccbc6049e87bbaa283e83fe052bd628c8c740
Atif Ahmad <40195767+AatifAhmad01@users.noreply.github.com>
2024-04-20 13:10:14 +0500

    add some effects

--- frontend-ecommerce/src/components/item/item.jsx
+++ frontend-ecommerce/src/components/item/item.jsx
@@ -8,7 +8,8 @@ export default function Item()
     return(
         <div className="item-container">
             <img src={itemImage} className="item-image"/>
-            <h3 className="item-heading">Rs.450</h3>
+            <h4 className="item-heading">Red Lady Lipstick</h4>
+            <h4 className="item-heading">Rs. 450</h4>
             <div className="buttons-container">
                 <button className="add-cart-button">Add To Cart</button>
                 <button className="favorite-button">O</button>
