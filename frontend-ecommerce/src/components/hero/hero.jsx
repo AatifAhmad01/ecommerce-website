623ef0a0a87705074d2f240d73f8ee8ee7a2b984
Atif Ahmad <40195767+AatifAhmad01@users.noreply.github.com>
2024-06-03 17:23:44 +0500

    fix carousal

--- frontend-ecommerce/src/components/hero/hero.jsx
+++ frontend-ecommerce/src/components/hero/hero.jsx
@@ -5,73 +5,130 @@ export default function Hero()
 {
 
     return(
-        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button
-            type="button"
-            data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide-to="0"
-            aria-label="Slide 1"
-            class="active prBgClr"
-            id="heroBtn"
-            ></button>
-            <button
-            type="button"
-            data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-            class=""
-            ></button>
-            <button
-            type="button"
-            data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-            aria-current="true"
-            ></button>
+        // <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        // <div class="carousel-indicators">
+        //     <button
+        //     type="button"
+        //     data-bs-target="#carouselExampleAutoplaying"
+        //     data-bs-slide-to="0"
+        //     aria-label="Slide 1"
+        //     class="active prBgClr"
+        //     id="heroBtn"
+        //     ></button>
+        //     <button
+        //     type="button"
+        //     data-bs-target="#carouselExampleAutoplaying"
+        //     data-bs-slide-to="1"
+        //     aria-label="Slide 2"
+        //     class=""
+        //     ></button>
+        //     <button
+        //     type="button"
+        //     data-bs-target="#carouselExampleAutoplaying"
+        //     data-bs-slide-to="2"
+        //     aria-label="Slide 3"
+        //     aria-current="true"
+        //     ></button>
 
+        // </div>
+        // <div class="carousel-inner">
+        //     <div class="carousel-item active" data-bs-interval="3000">
+        //         <div className="hero-container hero-item-first">
+
+        //         </div>
+        //     </div>
+
+        //     <div class="carousel-item" data-bs-interval="3000">
+        //         <div className="hero-container hero-item-second">
+
+        //         </div>
+
+        //     </div>
+
+        //     <div class="carousel-item" data-bs-interval="3000">
+        //         <div className="hero-container hero-item-third">
+
+        //         </div>
+
+        //     </div>
+
+        // </div>
+        // <button
+        //     class="carousel-control-prev"
+        //     type="button"
+        //     data-bs-target="#carouselExampleAutoplaying"
+        //     data-bs-slide="prev"
+        // >
+        //     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        //     <span class="visually-hidden">Previous</span>
+        // </button>
+        // <button
+        //     class="carousel-control-next"
+        //     type="button"
+        //     data-bs-target="#carouselExampleAutoplaying"
+        //     data-bs-slide="next"
+        // >
+        //     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        //     <span class="visually-hidden">Next</span>
+        // </button>
+        // </div> 
+
+
+        <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="3000">
-                <div className="hero-container hero-item-first">
-
+            <div class="carousel-item active hero-container" >
+                <img src="images/hero.jpg" class="d-block w-100" alt="..."></img>
+                <div class="carousel-caption d-none d-md-block" >
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
                 </div>
             </div>
-
-            <div class="carousel-item" data-bs-interval="3000">
-                <div className="hero-container hero-item-second">
-
-                </div>
-
+            <div class="carousel-item hero-container">
+            <img src="images/hero2.jpg" class="d-block w-100" alt="..."></img>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
+            </div>
+            <div class="carousel-item hero-container">
+            <img src="images/hero3.jpg" class="d-block w-100" alt="..."></img>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
             </div>
 
-            <div class="carousel-item" data-bs-interval="3000">
-                <div className="hero-container hero-item-third">
-
-                </div>
-
+            <div class="carousel-item hero-container">
+            <img src="images/hero3.jpg" class="d-block w-100" alt="..."></img>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
             </div>
 
+            <div class="carousel-item hero-container">
+            <img src="images/hero3.jpg" class="d-block w-100" alt="..."></img>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+            </div>
         </div>
-        <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="prev"
-        >
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="next"
-        >
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
-        </div> 
-
+        </div>
     )
 }
